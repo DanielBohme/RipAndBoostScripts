fefdd9983194a44b9c419c90d24bc8c19e28c1f9
DanielBohme <52354696+DanielBohme@users.noreply.github.com>
2019-10-03 18:01:38 +0200

    Added initial files

---
+++ Rotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    [SerializeField] Vector3 rotateVector = new Vector3(1f, 1f, 1f);
+    [SerializeField] float speed = 0.4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotateVector * speed * Time.deltaTime);
+    }
+}
